6fa1557b1b5d9f3820f29108696f159b9fa11a6d
Félix Piédallu <felix@piedallu.me>
2023-01-18 13:30:27 +0100

    Initial commit for enDI

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,29 +3,96 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-# PHP APP SPECIFIC
-#=================================================
-# Depending on its version, YunoHost uses different default PHP version:
-## YunoHost version "11.X" => PHP 7.4
-## YunoHost version "4.X"  => PHP 7.3
-#
-# This behaviour can be overridden by setting the YNH_PHP_VERSION variable
-#YNH_PHP_VERSION=7.3
-#YNH_PHP_VERSION=7.4
-#YNH_PHP_VERSION=8.0
-# For more information, see the PHP application helper: https://github.com/YunoHost/yunohost/blob/dev/helpers/php#L3-L6
-# Or this app package depending on PHP: https://github.com/YunoHost-Apps/grav_ynh/blob/master/scripts/_common.sh
-# PHP dependencies used by the app (must be on a single line)
-#php_dependencies="php$YNH_PHP_VERSION-deb1 php$YNH_PHP_VERSION-deb2"
-# or, if you do not need a custom YNH_PHP_VERSION:
-php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="deb1 deb2 $php_dependencies"
+pkg_dependencies=(
+    python3-pip
+    python3-venv
+    cython3
+
+    npm
+
+    python3-mysqldb
+    # libmariadbclient-dev
+    libmariadb-dev-compat
+    mariadb-client
+
+    libjpeg-dev libfreetype6 libfreetype6-dev
+    libssl-dev
+    libxml2-dev
+    zlib1g-dev
+
+    libxslt1-dev
+    libcairo2 libpango-1.0-0 libpangocairo-1.0-0
+)
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+__ynh_python_venv_setup() {
+    local -A args_array=( [d]=venv_dir= [p]=packages= )
+    local venv_dir
+    local packages
+    ynh_handle_getopts_args "$@"
+
+    python3 -m venv --system-site-packages "$venv_dir"
+
+    if [[ -n "${packages:-}" ]]; then
+        IFS=" " read -r -a pip_packages <<< "$packages"
+        "$venv_dir/bin/python3" -m pip install --upgrade pip "${pip_packages[@]}"
+    fi
+}
+
+__ynh_python_venv_get_site_packages_dir() {
+    local -A args_array=( [d]=venv_dir= )
+    local venv_dir
+    ynh_handle_getopts_args "$@"
+
+    "$venv_dir/bin/python3" -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'
+}
+
+#=================================================
+# Redis HELPERS
+#=================================================
+
+# get the first available redis database
+#
+# usage: ynh_redis_get_free_db
+# | returns: the database number to use
+ynh_redis_get_free_db() {
+    local result max db
+    result=$(redis-cli INFO keyspace)
+
+    # get the num
+    max=$(cat /etc/redis/redis.conf | grep ^databases | grep -Eow "[0-9]+")
+
+    db=0
+    # default Debian setting is 15 databases
+    for i in $(seq 0 "$max")
+    do
+        if ! echo "$result" | grep -q "db$i"
+        then
+            db=$i
+            break 1
+        fi
+        db=-1
+    done
+
+    test "$db" -eq -1 && ynh_die "No available Redis databases..."
+
+    echo "$db"
+}
+
+# Create a master password and set up global settings
+# Please always call this script in install and restore scripts
+#
+# usage: ynh_redis_remove_db database
+# | arg: database - the database to erase
+ynh_redis_remove_db() {
+    local db=$1
+    redis-cli -n "$db" flushall
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
